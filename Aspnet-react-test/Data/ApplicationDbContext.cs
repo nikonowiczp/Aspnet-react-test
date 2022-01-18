58a0525043d65b15fb9d3fda46cf8c13d943ad67
Patryk Nikonowicz <01151599@pw.edu.pl>
2022-01-18 23:44:46 +0100

    Initial commit

---
+++ Aspnet-react-test/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Aspnet_react_test.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aspnet_react_test.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
